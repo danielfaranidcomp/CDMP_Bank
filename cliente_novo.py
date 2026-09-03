1def029baf41db291e685e0457291be3b7813579
Cyberthoth1001 <santosantoniovinicius4@gmail.com>
2026-09-03 09:50:28 -0300

    Add files via upload

---
+++ cliente_novo.py
@@ -0,0 +1,11 @@
+nomeG=None
+senhaG=None
+emailG=None
+cpfG=None
+
+def criar_cliente(nome,senha,email,cpf):
+    global nomeG, senhaG, emailG, cpfG
+    nomeG=nome
+    senhaG=senha
+    emailG=email
+    cpfG=cpf
