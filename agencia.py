69eafdaf3cc754d25998d5f5381947909b7be100
Daniel <daniel.defarias@dcomp.ufs.br>
2026-09-23 18:24:14 -0300

    agência implementada, pequenos consertos em funções de conta. depois faço a função de montante da agência.

---
+++ agencia.py
@@ -0,0 +1,72 @@
+import json
+import conta
+
+""" agencia.json funcionará da seguinte forma: id da agência, número n de contas
+associadas à agência, próximos n itens serão a relação no seguinte formato:
+'idAgencia: idConta' """
+
+def criarNovaAgencia():
+    try:
+        with open('agencias.json', 'r', encoding="utf-8") as f:
+            dadosAgencia = json.load(f)
+        # descobrir o último id registrado
+        if dadosAgencia[-1] == 0: # verifica se a última agencia nao tem nenhuma conta associada
+            id = dadosAgencia[-2] + 1
+        else:
+            id = int(list(dadosAgencia[-1])[0]) + 1
+        dadosAgencia.extend([id, 0]) 
+        with open('agencias.json', 'w', encoding="utf-8") as f:
+            json.dump(dadosAgencia, f, indent=1, ensure_ascii=False)
+        return f"\nAgência cadastrada com sucesso. Id da agência: {id}.\n"
+    except:
+        with open('agencias.json', 'w', encoding="utf-8") as f:
+            json.dump([0, 0], f, indent=1, ensure_ascii=False)
+        return f"\nAgência cadastrada com sucesso. Id da agência: 0.\n"
+
+
+def associarContaAgencia(idAgencia, idConta, indexAgencia):
+    # não precisamos verificar se a conta nem a agencia existe porque essa 
+    # função só será chamado no momento da criação da conta, com a agencia verificada.
+    with open('agencias.json', 'r', encoding="utf-8") as f:
+        dadosAgencia = json.load(f)
+    # vamos fazer a inserção ORDENADA
+    indexFinal = indexAgencia + dadosAgencia[indexAgencia-1] 
+    dadosAgencia[indexAgencia-1] += 1 # aumenta o número n de contas associadas à agência
+    ehUltimo = True
+    for indexAgencia in range(indexAgencia, indexFinal):
+        idAtual = int(list(dadosAgencia[indexAgencia])[2])
+        if idConta < idAtual and ehUltimo: # esse ehUltimo é pra n repetir
+            dadosAgencia.insert(indexAgencia, f"{idAgencia}:{idConta}")
+            ehUltimo = False
+        elif (indexAgencia + 1) == indexFinal: # se está no final
+            indexAgencia += 1
+    if ehUltimo:
+        dadosAgencia.insert(indexAgencia, f"{idAgencia}:{idConta}")
+    with open('agencias.json', 'w', encoding="utf-8") as f:
+        json.dump(dadosAgencia, f, indent=1, ensure_ascii=False)
+    return f"\nConta de id {idConta} associada à agência de id {idAgencia} com sucesso.\n"
+
+
+def montanteAgencia(idAgencia):
+    # verifica se a agência exsite
+    try:
+        with open('agencias.json', 'r', encoding="utf-8") as f:
+            dadosAgencia = json.load(f)
+        # procurar e já salvar o index em indexAgencia
+        achou = False
+        i = 0
+        while i < len(dadosAgencia) and not achou:
+            if dadosAgencia[i] == idAgencia:
+                achou = True
+                i += 2
+            else:
+                i += dadosAgencia[i+1] + 2
+        if not achou:
+            return "\nErro! Agência não cadastrada,\n"
+    except:
+        return "\nErro! Agência não cadastrada.\n"
+
+    # começar a análise do montante:
+    if dadosAgencia[i-1]:
+        print("oi")
+        #depois termino
--- conta.py
+++ conta.py
@@ -1,24 +1,43 @@
 import json
+import agencia
 
 """ o padrão da lista de contas vai ser o seguinte:
     para cada conta, o primeiro item é o saldo da conta, o segundo o id,
-    o terceiro o número n de clientes associados. paralelamente,
-    há uma outra lista de contas associadas, com formato:
+    o terceiro o id da agencia, o quarto o número n de clientes associados. 
+    paralelamente, há uma outra lista de contas associadas, com formato:
     'idConta:idCliente' """
 
-def cadastrarConta(saldoInicial, idCliente):
+def cadastrarConta(saldoInicial, idCliente, idAgencia):
     # primeiro verifica se o cliente existe
     try: 
         with open('clientes.json', 'r', encoding="utf-8") as f:
             dadosClientes = json.load(f)
         achou = False #flag
         for cliente in range(0, len(dadosClientes), 4):
-            if str(idCliente) == str(dadosClientes[cliente]):
+            if idCliente == dadosClientes[cliente]:
                 achou = True
         if not achou:
-            return f"Erro! o cliente de id {idCliente} não existe."
+            return f"\nErro! o cliente de id {idCliente} não existe.\n"
     except:
-        return f"Erro! o cliente de id {idCliente} não existe."
+        return f"\nErro! o cliente de id {idCliente} não existe.\n"
+
+    # verifica se a agência exsite
+    try:
+        with open('agencias.json', 'r', encoding="utf-8") as f:
+            dadosAgencia = json.load(f)
+        # procurar e já salvar o index em indexAgencia
+        achou = False
+        i = 0
+        while i < len(dadosAgencia) and not achou:
+            if dadosAgencia[i] == idAgencia:
+                achou = True
+                i += 2
+            else:
+                i += dadosAgencia[i+1] + 2
+        if not achou:
+            return "\nErro! Agência não cadastrada,\n"
+    except:
+        return "\nErro! Agência não cadastrada.\n"
         
     # iniciando operação de cadastro da conta
     try:
@@ -27,21 +46,23 @@ def cadastrarConta(saldoInicial, idCliente):
             dadosContas = json.load(f)
         with open('relacaoContaCliente.json', 'r', encoding="utf-8") as f:
             dadosRelacaoContaCliente = json.load(f)
-        id = dadosContas[-3] + 1 # determinando o id da conta nova
-        dadosContas.extend([id, saldoInicial, 1]) # jogad tudo na lista
+        id = dadosContas[-4] + 1 # determinando o id da conta nova
+        dadosContas.extend([id, saldoInicial, idAgencia , 1]) # joga tudo na lista
         dadosRelacaoContaCliente.append(f"{id}:{idCliente}")
         #colocando de volta no json
         with open('contas.json', 'w', encoding="utf-8") as f:
             json.dump(dadosContas, f, indent=1, ensure_ascii=False)
         with open('relacaoContaCliente.json', 'w', encoding="utf-8") as f:
             json.dump(dadosRelacaoContaCliente, f, indent=1, ensure_ascii=False)
-        return f"Conta cadastrada com sucesso. id da conta: {id}." 
+        print(agencia.associarContaAgencia(idAgencia, id, i))
+        return f"\nConta cadastrada com sucesso. id da conta: {id}.\n" 
     except: # nesse caso aqui, vai ser o primeiro cliente a ser cadastrado
+        print(agencia.associarContaAgencia(idAgencia, 0, i))
         with open('contas.json', 'w', encoding="utf-8") as f:
-            json.dump([0, saldoInicial, 1], f, indent=1, ensure_ascii=False)
+            json.dump([0, saldoInicial, idAgencia, 1], f, indent=1, ensure_ascii=False)
         with open('relacaoContaCliente.json', 'w', encoding="utf-8") as f:
             json.dump([f"0:{idCliente}"], f, indent=1, ensure_ascii=False)
-            return "Conta cadastrada com sucesso. id da conta: 0."
+            return "\nConta cadastrada com sucesso. id da conta: 0.\n"
 
 
 def associarClienteConta(idConta, idCliente):
@@ -51,12 +72,12 @@ def associarClienteConta(idConta, idCliente):
             dadosClientes = json.load(f)
         achou = False #flag
         for cliente in range(0, len(dadosClientes), 4):
-            if str(idCliente) == str(dadosClientes[cliente]):
+            if idCliente == dadosClientes[cliente]:
                 achou = True
         if not achou:
-            return f"Erro! o cliente de id {idCliente} não existe."
+            return f"\nErro! o cliente de id {idCliente} não existe.\n"
     except:
-        return f"Erro! o cliente de id {idCliente} não existe."
+        return f"\nErro! o cliente de id {idCliente} não existe.\n"
 
     # segundo verifica se a conta existe ou nem
     # também já vai buscando o index que vai receber o insert na relacaoContaCliente
@@ -66,19 +87,19 @@ def associarClienteConta(idConta, idCliente):
         achou = False
         indexDoInsert = -1
         rangeFinalInsert = 0 #isso vai ser útil lá pra frente
-        for conta in range(0, len(dadosContas), 3):
-            if str(idConta) == str(dadosContas[conta]):
+        for conta in range(0, len(dadosContas), 4):
+            if idConta == dadosContas[conta]:
                 indexDoInsert += 1
-                rangeFinalInsert = indexDoInsert + dadosContas[conta+2]
+                rangeFinalInsert = indexDoInsert + dadosContas[conta+3]
                 achou = True
                 # aproveita e já aumenta o número da qntdd de clientes associados
-                dadosContas[conta+2] += 1
+                dadosContas[conta+3] += 1
             if not achou:
-                indexDoInsert += dadosContas[conta+2]
+                indexDoInsert += dadosContas[conta+3]
         if not achou:
-            return f"Erro! a conta de id {idConta} não existe."
+            return f"\nErro! a conta de id {idConta} não existe.\n"
     except:
-        return f"Erro! a conta de id {idConta} não existe."
+        return f"\nErro! a conta de id {idConta} não existe.\n"
 
     # vamos agora fazer a inserção ORDENADA e verificar se o já não está associado.
     # não cheguei a  usar try porque se as outras etapas deram certo, com certeza o arquivo existe entao...
@@ -86,12 +107,12 @@ def associarClienteConta(idConta, idCliente):
         dadosRelacaoContaCliente = json.load(f)
     ehUltimo = True #flag
     for i in range(indexDoInsert, rangeFinalInsert):
-        idAtual = list(dadosRelacaoContaCliente[i])[2]
+        idAtual = int(list(dadosRelacaoContaCliente[i])[2])
         if idCliente < idAtual:
             dadosRelacaoContaCliente.insert(i, f"{idConta}:{idCliente}")
             ehUltimo = False
         elif idAtual == idCliente:
-            return f"O id {idCliente} já está associado à conta."
+            return f"\nO id {idCliente} já está associado à conta.\n"
     if ehUltimo:
         dadosRelacaoContaCliente.insert(i+1, f"{idConta}:{idCliente}")
     # subindo lá dento
@@ -99,7 +120,7 @@ def associarClienteConta(idConta, idCliente):
         json.dump(dadosRelacaoContaCliente, f, indent=1, ensure_ascii=False)
     with open('contas.json', 'w', encoding="utf-8") as f:
         json.dump(dadosContas, f, indent=1, ensure_ascii=False)
-    return "Cliente associado à conta com sucesso"
+    return "\nCliente associado à conta com sucesso\n"
 
 
 def listarContas():
@@ -109,15 +130,16 @@ def listarContas():
         with open('relacaoContaCliente.json', 'r', encoding="utf-8") as f:
             dadosRelacaoContaCliente = json.load(f)
         contadorClienteConta = -1
-        for i in range(0, len(dadosConta), 3):
+        for i in range(0, len(dadosConta), 4):
             print(f"\nCONTA DE ID: {dadosConta[i]}")
             print(f"Saldo da conta: {dadosConta[i+1]:.2f}")
-            print("Id dos clientes associados à conta: ", end="")
-            for j in range(0, dadosConta[i+2]):
+            print(f"Id da agência da conta: {dadosConta[i+2]}")
+            print("Id do(s)) cliente(s) associado(s) à conta: ", end="")
+            for j in range(0, dadosConta[i+3]):
                 contadorClienteConta += 1
                 informacao = list(dadosRelacaoContaCliente[contadorClienteConta])
                 # só deixando bonitinho, separando por vírgula e ponto final tlgd
-                if str(j+1) != str(dadosConta[i+2]):
+                if str(j+1) != str(dadosConta[i+3]):
                     print(f"{informacao[2]}, ", end="")
                 else:
                     print(f"{informacao[2]}.")
@@ -134,7 +156,7 @@ def sacarConta(idConta, valor):
             dadosContas = json.load(f)
         achou = False
         index = 0
-        for i in range(0, len(dadosContas), 3):
+        for i in range(0, len(dadosContas), 4):
             if dadosContas[i] == idConta:
                 achou = True
                 index = i+1
@@ -161,7 +183,7 @@ def depositarConta(idConta, valor):
             dadosContas = json.load(f)
         achou = False
         index = 0
-        for i in range(0, len(dadosContas), 3):
+        for i in range(0, len(dadosContas), 4):
             if dadosContas[i] == idConta:
                 achou = True
                 index = i+1
@@ -179,24 +201,34 @@ def depositarConta(idConta, valor):
     return f"\nValor de R$ {valor:.2f} depositado com sucesso. Saldo atual: R$ {dadosContas[index]:.2f}\n"
 
 
-def consultarSaldo(idConta):
+# valorPuro = True retorna somente o número, valorPuro = False retorna uma frase.
+def consultarSaldo(idConta, valorPuro): 
     # primeiro vamos verificar se a conta existe e já salvar o endereço do saldo na lista
     try:
         with open('contas.json', 'r', encoding="utf-8") as f:
             dadosContas = json.load(f)
         achou = False
         index = 0
-        for i in range(0, len(dadosContas), 3):
+        for i in range(0, len(dadosContas), 4):
             if dadosContas[i] == idConta:
                 achou = True
                 index = i+1
         if not achou:
-            return f"\nErro! A conta não existe.\n"
+            if valorPuro:
+                return False
+            else:
+                return f"\nErro! A conta não existe.\n"
     except:
-        return f"\nErro! A conta não existe.\n"
+        if valorPuro:
+            return False
+        else:
+            return f"\nErro! A conta não existe.\n"
 
     # agora vamos retornar o valor so saldo
-    return f"\nO valor do saldo é R$ {dadosContas[index]:.2f}\n"
+    if valorPuro:
+        return dadosContas[index]
+    else:
+        return f"\nO valor do saldo é R$ {dadosContas[index]:.2f}\n"
 
 
 def montanteTotal():
@@ -204,7 +236,7 @@ def montanteTotal():
         with open('contas.json', 'r', encoding="utf-8") as f:
             dadosConta = json.load(f)
         total = 0
-        for i in range(1, len(dadosConta), 3):
+        for i in range(1, len(dadosConta), 4):
             total += dadosConta[i]
         return f"\nMontante total do banco: R$ {total:.2f}\n"
     except:
